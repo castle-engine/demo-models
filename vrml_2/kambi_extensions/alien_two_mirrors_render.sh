c83f6f2ff6e25cfc7a0ba7b829b1169e807f135b
Michalis Kamburelis <michalis.kambi@gmail.com>
2006-08-20 13:27:03 +0000

    * Setting of alien_mirror corrected a little to get better rendering, also rayhunter uses scene-bg-color now
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/kambi_vrml_examples@1129 afe8dcc5-9094-451b-8773-5f495042a737

--- vrml_2/kambi_extensions/alien_two_mirrors_render.sh
+++ vrml_2/kambi_extensions/alien_two_mirrors_render.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
 
-rayhunter classic 10 1600 1200 alien_mirror.wrl alien_two_mirrors_1.png
+rayhunter classic 10 1600 1200 alien_mirror.wrl alien_two_mirrors_1.png \
+   --scene-bg-color 0.057 0.221 0.4
 convert alien_two_mirrors_1.png \
   -filter Gaussian -geometry 800x600 alien_two_mirrors_2.png
