f9ef3a80300a529f3d7e457b89069d583cedef88
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-12-26 00:07:59 +0000

    * Fixed glsl_negative.fs shader to work on Radeon (Mac Book Pro, Mac OS X):   * you have to take ".st" of texture coord, otherwise no matching overloaded version of texture2D found   * write "1" as "vec4(1, 1, 1, 1)"
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_test_suite@2389 afe8dcc5-9094-451b-8773-5f495042a737

--- vrml_2/kambi_extensions/shaders/glsl_negative.fs
+++ vrml_2/kambi_extensions/shaders/glsl_negative.fs
@@ -2,6 +2,6 @@ uniform sampler2D tex;
 
 void main()
 {
-  vec4 texColor = texture2D(tex, gl_TexCoord[0]);
-  gl_FragColor = 1 - texColor * gl_Color;
+  vec4 texColor = texture2D(tex, gl_TexCoord[0].st);
+  gl_FragColor = vec4(1, 1, 1, 1) - texColor * gl_Color;
 }
