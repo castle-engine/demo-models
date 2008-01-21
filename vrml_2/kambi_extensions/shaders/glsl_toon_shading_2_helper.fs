edb653a45bce383243ce7d398304fd394905c125
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-01-21 21:54:38 +0000

    * Fixed shaders to avoid NVidia warnings "implicit cast from "int" to "float""
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_test_suite@2539 afe8dcc5-9094-451b-8773-5f495042a737

--- vrml_2/kambi_extensions/shaders/glsl_toon_shading_2_helper.fs
+++ vrml_2/kambi_extensions/shaders/glsl_toon_shading_2_helper.fs
@@ -2,12 +2,12 @@ vec4 color_from_intensity_original(float intensity)
 {
   // Decide which shade to used based on intensity
   if (intensity > 0.95)
-    return vec4(1, 1, 0, 1); else
+    return vec4( 1.0,  1.0, 0.0, 1.0); else
   if (intensity > 0.5)
-    return vec4(0.75, 0.75, 0, 1); else
+    return vec4(0.75, 0.75, 0.0, 1.0); else
   if (intensity > 0.25)
-    return vec4(0.5, 0.5, 0, 1); else
-    return vec4(0.25, 0.25, 0, 1);
+    return vec4( 0.5,  0.5, 0.0, 1.0); else
+    return vec4(0.25, 0.25, 0.0, 1.0);
 }
 
 /* Kambi note: color_from_intensity_original doesn't work like it should on
@@ -35,13 +35,13 @@ vec4 color_from_intensity_alt(float intensity)
    and avoiding fglrx problems. */
 vec4 color_from_intensity(float intensity)
 {
-  vec4 result = vec4(1, 1, 0, 1);
-  if (intensity * 100 <= 95)
+  vec4 result = vec4(1.0, 1.0, 0.0, 1.0);
+  if (intensity * 100.0 <= 95.0)
   {
     result /= 4.0;
-    if (intensity * 100 > 50)
+    if (intensity * 100.0 > 50.0)
       result *= 3.0; else
-    if (intensity * 100 > 25)
+    if (intensity * 100.0 > 25.0)
       result *= 2.0;
     /* else stays as vec4(0.25, 0.25, 0, 1); */
   }
--- vrml_2/kambi_extensions/shaders/glsl_toon_shading_per_vertex.fs
+++ vrml_2/kambi_extensions/shaders/glsl_toon_shading_per_vertex.fs
@@ -2,13 +2,13 @@ varying float intensity;
 
 vec4 color_from_intensity(float intensity)
 {
-  vec4 result = vec4(1, 1, 0, 1);
-  if (intensity * 100 <= 95)
+  vec4 result = vec4(1.0, 1.0, 0.0, 1.0);
+  if (intensity * 100.0 <= 95.0)
   {
     result /= 4.0;
-    if (intensity * 100 > 50)
+    if (intensity * 100.0 > 50.0)
       result *= 3.0; else
-    if (intensity * 100 > 25)
+    if (intensity * 100.0 > 25.0)
       result *= 2.0;
     /* else stays as vec4(0.25, 0.25, 0, 1); */
   }
