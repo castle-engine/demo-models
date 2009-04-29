a07e598163011cc814dca96e9fd9afe535d5b20d
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-04-29 22:07:14 +0000

    * improved shadow_map_test.x3dv: more test teapots, GLSL shader adds a little ambient term
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_test_suite@4518 afe8dcc5-9094-451b-8773-5f495042a737

--- x3d/shadow_map.fs
+++ x3d/shadow_map.fs
@@ -3,4 +3,6 @@ uniform sampler2DShadow shadowMap;
 void main(void)
 {
   gl_FragColor = shadow2DProj(shadowMap, gl_TexCoord[0]).r * gl_Color;
+  /* add some ambient term */
+  gl_FragColor += vec4(0.25, 0.25, 0.25, 1);
 }
