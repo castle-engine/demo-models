25610a638b5fdeeee6e11eb5b300f2c34fc01069
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-04-21 15:14:24 +0000

    * make screenshot for paper with teapot from simple_shadow_map_teapots.x3dv
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_test_suite@5923 afe8dcc5-9094-451b-8773-5f495042a737

--- x3d/shadow_maps/shadow_map_pcf16.fs
+++ x3d/shadow_maps/shadow_map_pcf16.fs
@@ -29,4 +29,16 @@ void main(void)
       shadow2DProj(shadowMap, gl_TexCoord[0] + vec4( offset * 0.5, -offset * 1.5, 0.0, 0.0)).r
     )
     / 16.0 ) * gl_Color;
+
+  /* Uncomment to keep some non-zero color.
+
+     (Teoretically, scaling gl_Color makes no sense. If the light
+     is shadowed, then it doesn't light at all -> so it should not be scaled,
+     it should be set to zero. Ambient term, if any, should only be added.
+     But in practice, this hides the model shape in the shadow,
+     so it's ugly.
+
+     So we fake global lighting nicer by scaling gl_Color.)
+  */
+  /*  gl_FragColor += gl_Color / 5.0; */
 }
