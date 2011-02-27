ae24153e7d07f82bf7d7ddb48eb091eb732da21e
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-02-27 07:50:33 +0000

    Rename PLUG_fragment_normal_eye to PLUG_fragment_eye_space, pass there also vertex. This is consistent with PLUG_vertex_eye_space.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/demo_models@8538 afe8dcc5-9094-451b-8773-5f495042a737

--- water/water_normal.fs
+++ water/water_normal.fs
@@ -1,7 +1,7 @@
 varying mat3 normal_matrix;
 varying vec2 normalMapTexCooord;
 
-void PLUG_fragment_normal_eye(inout vec3 normal)
+void PLUG_fragment_eye_space(const vec4 vertex, inout vec3 normal)
 {
   normal = vec3(0.0, 0.0, 1.0);
   /* PLUG: water_normal_object_space (normal, normalMapTexCooord) */
