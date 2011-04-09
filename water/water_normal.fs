6d2e5f81d2ef6fa9d7674013f32f70eed8f9aea8
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-04-09 21:54:41 +0000

    Invert normal.z properly for water.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/demo_models@8744 afe8dcc5-9094-451b-8773-5f495042a737

--- water/water_normal.fs
+++ water/water_normal.fs
@@ -5,5 +5,7 @@ void PLUG_fragment_eye_space(const vec4 vertex, inout vec3 normal)
 {
   normal = vec3(0.0, 0.0, 1.0);
   /* PLUG: water_normal_object_space (normal, normalMapTexCooord) */
+  if (!gl_FrontFacing) normal.z = -normal.z;
+
   normal = normal_matrix * normal;
 }
