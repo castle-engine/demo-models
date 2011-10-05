fc88639e0acfebeba4f110dce5c8b5e283e9ac36
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-10-05 02:53:46 +0000

    Rename kambi.cfg -> castle-fpc.cfg, KAMBI_FPC_OPTIONS -> CASTLE_FPC_OPTIONS, fix comments in many xxx_compile.sh scripts.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/demo_models@10168 afe8dcc5-9094-451b-8773-5f495042a737

--- shadow_maps/sunny_street/sunny_street_process_compile.sh
+++ shadow_maps/sunny_street/sunny_street_process_compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu
 
+# Call this script from this directory,
+# or from base castle_game_engine directory.
+# Or just do "make examples" in base castle_game_engine directory.
+
 # Allow calling this script from it's dir.
-if [ -f sunny_street_process.lpr ]; then
-  cd ../../../castle_game_engine/
-fi
+if [ -f sunny_street_process.lpr ]; then cd ../../../castle_game_engine/; fi
 
-# Call this from ../../ (or just use `make examples').
-fpc -dRELEASE @kambi.cfg ../demo_models/shadow_maps/sunny_street/sunny_street_process.lpr
-
+fpc -dRELEASE @castle-fpc.cfg ../demo_models/shadow_maps/sunny_street/sunny_street_process.lpr
