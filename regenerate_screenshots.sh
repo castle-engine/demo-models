6188111a141f59879499ab5be818a74caadc183c
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-04-05 01:23:41 +0200

    Script to regenerate screenshots

---
+++ regenerate_screenshots.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -eu
+
+# ----------------------------------------------------------------------------
+# Regenerate, using view3dscene, proper screenshots for all models.
+# Run with
+# - no arguments (regenerate everything here)
+# - directory name as argument (regenerate everything there, recursively)
+# - file name as argument (regenerate this one file)
+# ----------------------------------------------------------------------------
+
+# Allows to properly handle filenames with spaces,
+# "for" iteration over a filename with 1-filename per line will be reliable.
+IFS=$'\n'
+
+# Functions ------------------------------------------------------------------
+
+# Regenerate screenshot for file $1
+do_file ()
+{
+  MODEL_FILE="$1"
+
+  # use bash expansion to strip extensions,
+  # even double extensions like .wrl.gz,
+  # see https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
+  # and https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html#Shell-Parameter-Expansion
+  SCREENSHOT_FILE="${1%%.*}.png"
+
+  echo "Making screenshot ${MODEL_FILE} -> ${SCREENSHOT_FILE}"
+  view3dscene "${MODEL_FILE}" --geometry 1600x900 --screenshot 0 "${SCREENSHOT_FILE}"
+}
+
+# Regenerate screenshot for everything in directory $1
+do_dir ()
+{
+  find "$1" \
+    '(' -type d -iname 'errors' -prune ')' -or \
+    '(' -type f -name '*test_temporary*' ')' -or \
+    '(' -type f '(' -iname '*.wrl' -or \
+                    -iname '*.wrz' -or \
+                    -iname '*.wrl.gz' -or \
+                    -iname '*.x3d' -or \
+                    -iname '*.x3dz' -or \
+                    -iname '*.x3d.gz' -or \
+                    -iname '*.x3dv' -or \
+                    -iname '*.x3dvz' -or \
+                    -iname '*.x3dv.gz' -or \
+                    -iname '*.3ds' -or \
+                    -iname '*.dae' -or \
+                    -iname '*.gltf' -or \
+                    -iname '*.glb' -or \
+                    -iname '*.plist' -or \
+                    -iname '*.starling-xml' ')' \
+                -print ')' > \
+          /tmp/cge-demo-models-regenerate-screenshots.sh.txt
+  echo 'Found files: '`wc -l < /tmp/cge-demo-models-regenerate-screenshots.sh.txt`
+  for FILE in `cat /tmp/cge-demo-models-regenerate-screenshots.sh.txt`; do
+    do_file "$FILE"
+  done
+}
+
+# main -----------------------------------------------------------------------
+
+if [ $# = 0 ]; then
+  do_dir .
+else
+  if [ -f "$1" ]; then
+    do_file "$1"
+  else
+    do_dir "$1"
+  fi
+fi
