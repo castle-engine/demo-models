1fa0e55c8e041e003c22c7f233ad082ed6237295
John W Carlson <yottzumm@gmail.com>
2025-08-12 04:10:51 -0500

    Updated batch convert

--- batchconvert.sh
+++ batchconvert.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
-for ext in x3d wrl x3dv gltf glb
+for ext in dae
+do
+	for i in `find . -name "*.${ext}"`
+	do
+		echo $i
+		~/Downloads/castle-model-viewer-5.3.0-win64-x86_64/castle-model-viewer/castle-model-converter.exe $i `dirname $i`/`basename $i .${ext}`.x3d
+	done
+done
+
+for ext in x3d x3dv gltf glb
 do
 	for i in `find . -name "*.${ext}"`
 	do
