ec58a9cedc54ca75795231a9f724667805288335
John W Carlson <yottzumm@gmail.com>
2025-08-12 04:25:19 -0500

    Remove vrml_1 paths

--- batchconvert.sh
+++ batchconvert.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 for ext in dae
 do
-	for i in `find . -name "*.${ext}"`
+	for i in `find . -name "*.${ext}"| grep -v vrml_1`
 	do
 		echo $i
 		~/Downloads/castle-model-viewer-5.3.0-win64-x86_64/castle-model-viewer/castle-model-converter.exe $i `dirname $i`/`basename $i .${ext}`.x3d
